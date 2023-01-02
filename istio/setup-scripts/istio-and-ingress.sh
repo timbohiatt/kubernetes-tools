3483595d53d82f8d4773d9b20c3417e0bb07353c
Tim Hiatt <timhiatt@google.com>
2023-01-02 08:46:17 +0000

    Adding Istio Raw Startup Scripts

--- istio/setup-scripts/istio-and-ingress.sh
+++ istio/setup-scripts/istio-and-ingress.sh
@@ -5,13 +5,17 @@
 #   It expects thats KubeConfig and Context is already configured.
 #
 
+# Setup Istio & Istio Ingress Gateway
+echo "[INSTALL] - Install Istio"
 # Setup Istio 
 helm repo add istio https://istio-release.storage.googleapis.com/charts
 helm repo update
 kubectl create namespace istio-system
 helm upgrade istio-base istio/base -n istio-system --install
 helm upgrade istiod istio/istiod -n istio-system --install --wait
+
 # Setup Istio Gateway
+echo "[INSTALL] - Install Istio Gateway"
 kubectl create namespace istio-ingress
 kubectl label namespace istio-ingress istio-injection=enabled
 helm install istio-ingress istio/gateway -n istio-ingress --wait
