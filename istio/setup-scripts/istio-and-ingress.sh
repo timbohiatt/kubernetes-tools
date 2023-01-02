0a25d649ba854f52ef78c04e032e694f81bb1d14
Tim Hiatt <timhiatt@google.com>
2023-01-02 13:06:46 +0000

    Adding some patches to Istio Install

--- istio/setup-scripts/istio-and-ingress.sh
+++ istio/setup-scripts/istio-and-ingress.sh
@@ -13,13 +13,12 @@ helm repo update
 kubectl create namespace istio-system
 helm upgrade istio-base istio/base -n istio-system --install
 helm upgrade istiod istio/istiod -n istio-system --install --wait
-helm upgrade istiod istio/istiod -n istio-system --install --wait
 
 #Sleep
 sleep 60s
 
 # Setup Istio Gateway
 echo "[INSTALL] - Install Istio Gateway"
-kubectl create namespace istio-ingress
-kubectl label namespace istio-ingress istio-injection=enabled
-helm install istio-ingress istio/gateway -n istio-ingress --wait
+#kubectl create namespace istio-ingress
+#kubectl label namespace istio-ingress istio-injection=enabled
+helm install istio-ingress istio/gateway -n istio-system --wait
